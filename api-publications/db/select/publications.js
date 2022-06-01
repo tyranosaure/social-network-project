bd8b0cf2de472302286c1d532b33e36b925c7fd9
marintosti12 <marin.tostivint@epsi.fr>
2022-06-01 15:09:17 +0200

    ..\api-comments\package-lock.json

--- api-publications/db/select/publications.js
+++ api-publications/db/select/publications.js
@@ -11,10 +11,23 @@ module.exports.findOne = function (client, req, res) {
 	})
 }
 
-module.exports.getPublications = function (client, req, res) {
+module.exports.getPublicationsForPagination = function (client, req, res) {
 	let limit = parseInt(req.params.limit)
 	let offset = parseInt(req.params.offset)
 	let query = `Select * from "post" by offset ${offset} limit ${limit}`
+	client.query(query, (err, result) => {
+		if (!err) {
+			res.status(200)
+			res.send(result.rows)
+		} else {
+			console.log(err.message)
+		}
+	})
+}
+
+
+module.exports.getPublications = function (client, req, res) {
+	let query = `Select * from "post"`
 	client.query(query, (err, result) => {
 		if (!err) {
 			res.status(200)
--- api-publications/routes/publications/get.js
+++ api-publications/routes/publications/get.js
@@ -1,7 +1,13 @@
-const { getPublications } = require("../../db/select/publications")
+const { getPublications, getPublicationsForPagination } = require("../../db/select/publications")
 
 module.exports = function (app, client) {
 	app.get("/publications/:limit/:offset", (req, res) => {
+		getPublicationsForPagination(client, req, res)
+		client.end
+	})
+
+
+	app.get("/publications", (req, res) => {
 		getPublications(client, req, res)
 		client.end
 	})
